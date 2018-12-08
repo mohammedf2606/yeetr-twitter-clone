0fcd53175d7f0c7ac202346e49e0414a436ed411
Eugene Lin <me@eugenel.in>
2018-12-08 20:51:28 +0000

    Moved functions

--- html/assets/includes/config.php
+++ html/assets/includes/config.php
@@ -22,4 +22,13 @@
     $q = $db->query("SELECT * FROM sessions WHERE sid='$token' AND state=1");
     return mysqli_num_rows($q) > 0;
   }
+  function generateRandomString($length) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+  }
 ?>
