7e9818cd1eb9a4f7dc7fa2c16f10a92c63a9cdf8
Raihaan Rahman <raihaan2000@gmail.com>
2018-12-08 15:08:17 +0000

    test for log in

--- html/index.php
+++ html/index.php
@@ -1,5 +1,27 @@
+<head>
+<title>YEET</title>
+<style>
+body {
+    background-color:#FFF1D2;
+}
+</style>
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+<meta name="google-signin-client_id" content="258775497785-n1gqf6u0q55et082h1roo46v6ci8c5up.apps.googleusercontent.com">
+</head>
+
+
+<body>
+<label>hello</label>
+<img src="logo1.gif" alt="YEET">
+<!--
+<a href="https://google.com"><img src="logo1.gif" alt="YEET"></a>
+-->
+<div class="g-signin2" data-onsuccess="onSignIn"></div>
+
 <?php
-  echo("Test");
-  echo nl2br("\r\nHello Fahim was here!");
-  echo "Hello";
-?>
+    echo("Test");
+    echo nl2br("\r\nHello Fahim was here!");
+    echo "Hello";
+    ?>
+
+</body>
