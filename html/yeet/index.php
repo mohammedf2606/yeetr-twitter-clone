b7d52e2044456573175867ef97d0df3257afd292
Raihaan Rahman <raihaan2000@gmail.com>
2018-12-08 22:05:28 +0000

    started the yeeting algorithm

--- html/yeet/index.php
+++ html/yeet/index.php
@@ -17,18 +17,34 @@
 
     <img src="/assets/img/logo.gif" alt="YEET"> <!-- Source: cooltext -->
     <img src="<?php echo $user['pic']; ?>">
-    <b><?php echo $user['name']; ?></b>
-    <h1> Handle </h1>
-    
+    <b class="name"><?php echo $user['name']; ?></b>
+    <label class="handle">Handle</label>
+
     <textarea placeholder="Type to Yeet!" id="yeetInput" rows="10" cols="80"></textarea>
     <p></p>
     <button type="button" onclick="submitYeet()">YEET!</button>
 
     <script>
+        function post(text){
+            var form = document.createElement("form");
+            form.setAttribute("method", "post");
+            form.setAttribute("action", "http://www.yeetr.me/endpoints/yeet.php");
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("value", text);
+            form.appendChild(hiddenField);
+            
+            document.body.appendChild(form);
+            form.submit();
+        }
         function submitYeet() {
             var textInput = document.getElementById("yeetInput").value;
-            
-            console.log(textInput);
+            if (textInput != "") {
+                console.log(textInput);
+                post(textInput);
+            } else {
+                console.log("no input found :(");
+            }
         }
     </script>
 
