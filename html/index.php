f87d99fd8cb2b85b4f59c3ef3a9cda82c1e823cb
Raihaan Rahman <raihaan2000@gmail.com>
2018-12-08 15:37:31 +0000

    testing getting user info

--- html/index.php
+++ html/index.php
@@ -19,4 +19,17 @@
 -->
     <div class="g-signin2" data-onsuccess="onSignIn"></div> <!-- Login button -->
 
+    <script> 
+        if (auth2.isSignedIn.get()) {
+        var profile = auth2.currentUser.get().getBasicProfile();
+        console.log('ID: ' + profile.getId());
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+}
+    
+    </script>
+
 </body>
