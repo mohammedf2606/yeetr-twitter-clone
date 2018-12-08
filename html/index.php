86d04d2b9876b4dded9aa93a4f7b4b75fb2e3610
Raihaan Rahman <raihaan2000@gmail.com>
2018-12-08 15:44:02 +0000

    used another method to get user id

--- html/index.php
+++ html/index.php
@@ -20,15 +20,18 @@
     <div class="g-signin2" data-onsuccess="onSignIn"></div> <!-- Login button -->
 
     <script>
-        if (auth2.isSignedIn.get()) {
-          var profile = auth2.currentUser.get().getBasicProfile();
-          console.log('ID: ' + profile.getId());
-          console.log('Full Name: ' + profile.getName());
-          console.log('Given Name: ' + profile.getGivenName());
-          console.log('Family Name: ' + profile.getFamilyName());
-          console.log('Image URL: ' + profile.getImageUrl());
-          console.log('Email: ' + profile.getEmail());
-        }
+        gapi.load('auth2', function() {
+            auth2 = gapi.auth2.init({
+                client_id: 'CLIENT_ID.apps.googleusercontent.com',
+                fetch_basic_profile: false,
+                scope: 'profile'
+            });
+
+        // Sign the user in, and then retrieve their ID.
+            auth2.signIn().then(function() {
+                console.log(auth2.currentUser.get().getId());
+            });
+        });
     </script>
 
 </body>
