11817962c710a412f4c6b9f231829e26979c0e4c
Raihaan Rahman <raihaan2000@gmail.com>
2018-12-08 18:07:21 +0000

    fifth attempt to initialise gapi

--- html/feed/index.php
+++ html/feed/index.php
@@ -19,19 +19,18 @@
 <body>
 
     <script>
-        gapi.auth2.init()
-        if (auth2.isSignedIn.get()) {
-            var profile = auth2.currentUser.get().getBasicProfile();
-            console.log('ID: ' + profile.getId());
-            console.log('Full Name: ' + profile.getName());
-            console.log('Given Name: ' + profile.getGivenName());
-            console.log('Family Name: ' + profile.getFamilyName());
-            console.log('Image URL: ' + profile.getImageUrl());
-            console.log('Email: ' + profile.getEmail());
-        } else {
-            console.log('Fail');
-        }
-        
+        gapi.load('auth2', function() {
+            auth2 = gapi.auth2.init({
+            client_id: 'CLIENT_ID.apps.googleusercontent.com',
+            fetch_basic_profile: false,
+            scope: 'profile'
+        });
+
+        // Sign the user in, and then retrieve their ID.
+        auth2.signIn().then(function() {
+            console.log(auth2.currentUser.get().getId());
+        });
+    });
     </script>
 
 </body>
