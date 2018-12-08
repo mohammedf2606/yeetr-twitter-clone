28ba102cac0d7067166918e63353113f7d17132b
Eugene Lin <me@eugenel.in>
2018-12-08 20:44:32 +0000

    Profile picture update on new auth

--- html/auth/index.php
+++ html/auth/index.php
@@ -11,12 +11,12 @@
       $user_id = strval($payload['sub']);
       $user_check = $db->query("SELECT * FROM users WHERE uid='$user_id'");
       $timestamp = strval(time());
+      $pp = $db->real_escape_string(strval($payload['picture']));
       if (mysqli_num_rows($user_check) == 0) {
         $name = $db->real_escape_string(strval($payload['name']));
-        $pp = $db->real_escape_string(strval($payload['picture']));
         $db->query("INSERT INTO users (uid, name, bio, web, reg, last_seen, pic) VALUES ('$user_id', '$name', 'I\'m new to Yeetr!', '', $timestamp, $timestamp, '$pp')") or die("{\"status\":0,\"content\":\"Session creation failed\"}");
       } else {
-        $db->query("UPDATE users SET last_seen=$timestamp WHERE uid='$user_id'") or die("{\"status\":0,\"content\":\"Session creation failed\"}");
+        $db->query("UPDATE users SET last_seen=$timestamp, pic='$pp' WHERE uid='$user_id'") or die("{\"status\":0,\"content\":\"Session creation failed\"}");
       }
       $sid = '';
       while (true) {
