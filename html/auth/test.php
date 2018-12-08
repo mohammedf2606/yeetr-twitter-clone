fb8b2dd09efa1a57e7541511275528a02acc02f4
Eugene Lin <me@eugenel.in>
2018-12-08 20:30:48 +0000

    Debugging, or something

--- html/auth/test.php
+++ html/auth/test.php
@@ -1,9 +1,9 @@
 <?php
   include("../assets/includes/config.php");
-  include("../assets/includes/composer/vendor/autoload.php");
   session_start();
   echo $_SESSION['sid'];
   $token = $db->real_escape_string($_SESSION['sid']);
   $q = $db->query("SELECT * FROM sessions WHERE sid='$token' AND status=1");
-  return mysqli_num_rows($q) > 0;
+  echo $q;
+  echo mysqli_num_rows($q) > 0;
 ?>
