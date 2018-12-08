5bddb6a554a875e6a5dd3b034e6656a05d952c51
Eugene Lin <me@eugenel.in>
2018-12-08 22:34:50 +0000

    Error numbers for debugging

--- html/endpoints/follow.php
+++ html/endpoints/follow.php
@@ -8,19 +8,19 @@
     $uid = $user['id'];
     $tid = $db->real_escape_string($_GET['target']);
     $timestamp = strval(time());
-    $db->query("UPDATE users SET last_seen=$timestamp WHERE uid='$uid'") or die("{\"status\":0,\"content\":\"Following failed\"}");
-    $user_check = $db->query("SELECT * FROM users WHERE uid='$tid'") or die("{\"status\":0,\"content\":\"Following failed\"}");
+    $db->query("UPDATE users SET last_seen=$timestamp WHERE uid='$uid'") or die("{\"status\":0,\"content\":\"Following failed0\"}");
+    $user_check = $db->query("SELECT * FROM users WHERE uid='$tid'") or die("{\"status\":0,\"content\":\"Following failed1\"}");
     if (mysqli_num_rows($user_check) == 0) {
       die("{\"status\":0,\"content\":\"Invalid target\"}");
     } else {
-      $follow_check = $db->query("SELECT * FROM follows WHERE user1='$uid' AND user2='$tid'") or die("{\"status\":0,\"content\":\"Following failed\"}");
+      $follow_check = $db->query("SELECT * FROM follows WHERE user1='$uid' AND user2='$tid'") or die("{\"status\":0,\"content\":\"Following failed2\"}");
       if (mysqli_num_rows($follow_check) == 0) {
-        $db->query("INSERT INTO follows (user1, user2) VALUES ('$uid', $tid')") or die("{\"status\":0,\"content\":\"Following failed\"}");
+        $db->query("INSERT INTO follows (user1, user2) VALUES ('$uid', $tid')") or die("{\"status\":0,\"content\":\"Following failed3\"}");
         die("{\"status\":1,\"content\":\"Followed\"}");
       } else {
         $follow = $follow_check->fetch_assoc();
         $fid = $follow['fid'];
-        $db->query("DELETE FROM follows WHERE fid='$fid'") or die("{\"status\":0,\"content\":\"Following failed\"}");
+        $db->query("DELETE FROM follows WHERE fid='$fid'") or die("{\"status\":0,\"content\":\"Following failed4\"}");
         die("{\"status\":1,\"content\":\"Unfollowed\"}");
       }
     }
