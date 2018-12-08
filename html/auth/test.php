9e40121d9924026c591e279f8f2efbfe18f8ec52
Eugene Lin <me@eugenel.in>
2018-12-08 20:28:16 +0000

    Testing session validity

--- html/auth/test.php
+++ html/auth/test.php
@@ -3,5 +3,7 @@
   include("../assets/includes/composer/vendor/autoload.php");
   session_start();
   echo $_SESSION['sid'];
-  echo validToken($_SESSION['sid']);
+  $token = $db->real_escape_string($_SESSION['sid']);
+  $q = $db->query("SELECT * FROM sessions WHERE sid='$token' AND status=1");
+  return mysqli_num_rows($q) > 0;
 ?>
