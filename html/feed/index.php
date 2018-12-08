843a5867bc6851a3de418ea996b88d3c474f5cc7
Raihaan Rahman <raihaan2000@gmail.com>
2018-12-08 17:45:45 +0000

    second attempt to initialise gapi

--- html/feed/index.php
+++ html/feed/index.php
@@ -6,12 +6,12 @@
     }
     </style>
     <meta name="google-signin-scope" content="profile email">
-    <script src="https://apis.google.com/js/platform.js" async defer>
+    <script src="https://apis.google.com/js/platform.js?onload=init" async defer>
     
         function init() {
             gapi.load('auth2', function() { // Ready. });
         }
-        
+
     </script>
     <meta name="google-signin-client_id" content="258775497785-n1gqf6u0q55et082h1roo46v6ci8c5up.apps.googleusercontent.com">
 </head>
