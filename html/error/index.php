51ebc12cca9d598e90e82d06d4816cfce7ce9e24
Eugene Lin <me@eugenel.in>
2018-12-09 02:49:42 +0000

    Error page

---
+++ html/error/index.php
@@ -0,0 +1,15 @@
+<?php
+  include("../assets/includes/config.php");
+?>
+<html>
+  <head>
+    <title>Error - <?php echo $conf_name; ?></title>
+    <link type="text/css" rel="stylesheet" href="../assets/css/master.css<?php if ($conf_refresh) { echo "?t=".strval(time()); } ?>">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  </head>
+  <body>
+    <img src="/assets/img/logo.gif" alt="YEET"> <!-- Source: cooltext -->
+    <h1>page not found!</h1>
+    <a href="../">Click here to return</a>
+  </body>
+</html>
