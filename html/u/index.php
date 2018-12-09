8e8b8247b942cfc91e5bf3cbe27004bf2de3bfc7
Eugene Lin <me@eugenel.in>
2018-12-09 08:26:57 +0000

    Reload after logout

--- html/u/index.php
+++ html/u/index.php
@@ -135,6 +135,11 @@
               var xhr = new XMLHttpRequest();
               xhr.open('GET', 'http://yeetr.me/endpoints/deauth.php');
               xhr.send();
+              xhr.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                  location.reload(true);
+                }
+            };
             }
           </script>
 <?php
