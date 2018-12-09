f0839d7e033c2a8ec7567ec9420816b80922ff82
Eugene Lin <me@eugenel.in>
2018-12-09 00:07:21 +0000

    Modifying table

--- html/feed/index.php
+++ html/feed/index.php
@@ -28,11 +28,14 @@
               <img style="vertical-align:top" src="<?php echo $user['pic']; ?>">
             </td>
             <td>
-              <b class = "name"><?php echo $user['name']; ?></b>
-              <label class = "handle"> Handle </label>
-              <label class = "time"> Time </label>
-              <br>
-              <label class = "yeet"> Yeet </label>
+              <div style="style="vertical-align:top">
+                <b class = "name"><?php echo $user['name']; ?></b>
+                <label class = "handle"> Handle </label>
+                <label class = "time"> Time </label>
+              </div>
+              <div style="style="vertical-align:bottom">
+                <label class = "yeet"> Yeet </label>
+              </div>
             </td>
           </tr>
         </table>
