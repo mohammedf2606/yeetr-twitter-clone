5258d543a552a1ec6119e7196b2e5c9dcfc020f6
Fahim Mohammed <mohammedf2606@gmail.com>
2018-12-09 05:06:57 +0000

    scope problems ffs

--- html/feed/index.php
+++ html/feed/index.php
@@ -18,14 +18,16 @@
       Search:<br>
       <input type="text"  name="search" placeholder="Search users" size=100>
       <button id ="search" onclick="search()"> Search! </button>
+      <script>
+        function search() {
+          console.log("Searching");
+        };
+      </script>
     </form>
     <center>
       <button id="newYeet" onclick="goToYeet()"> New Yeet :) </button>
     </center>
     <script>
-      function search() {
-        console.log("Searching");
-      };
       function goToYeet() {
         window.location.replace("../yeet");
       };
