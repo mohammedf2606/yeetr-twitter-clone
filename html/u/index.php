d981be6385e0ba3536c20d29985e880f83edcfef
Raihaan Rahman <raihaan2000@gmail.com>
2018-12-09 08:53:14 +0000

    replaced get with post

--- html/u/index.php
+++ html/u/index.php
@@ -132,7 +132,7 @@
               var auth2 = gapi.auth2.init({clientId: "<?php echo $client_id; ?>"});
                 auth2.signOut().then(function () {
                 var xhr = new XMLHttpRequest();
-                xhr.open('GET', 'http://yeetr.me/endpoints/deauth.php');
+                xhr.open('POST', 'http://yeetr.me/endpoints/deauth.php'); //replace post with get
                 xhr.send();
                 xhr.onreadystatechange = function() {
                   if (this.readyState == 4 && this.status == 200) {
